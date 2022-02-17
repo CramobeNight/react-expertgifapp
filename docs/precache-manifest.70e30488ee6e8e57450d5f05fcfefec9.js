330903d3faf6bd64aa0ee6ee437bfc18e2d1577e
CramobeNight <cramobenight07@gmail.com>
2022-02-17 15:58:45 -0500

    Agregamos docs

---
+++ docs/precache-manifest.70e30488ee6e8e57450d5f05fcfefec9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c790c1789698b3a9c26326e58efafd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f64ff40c7351923b622",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f64ff40c7351923b622",
+    "url": "/static/js/main.e2f85828.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
